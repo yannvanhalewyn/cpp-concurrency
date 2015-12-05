e6ccf877fb24950a037aa2ea4e75447417292e22
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-12-05 05:04:47 +0100

    Add running with 3 threads

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,57 @@ void sum(int from, int to, long* out) {
     for (int i = from; i <= to; i++) {
         sum += i;
     }
+
     *out = sum;
 }
 
 int main(int argc, char *argv[])
 {
-    // Multithreaded
-    std::cout << "Running example with 2 threads" << std::endl;
+    // Single Threaded
+    std::cout << "Running single threaded example: " << std::endl;
     TimeDifference* timer = new TimeDifference();
     timer->begin();
+    long *out = new long;
+    sum(0, numberToSumUp, out);
+    timer->end();
+    std::cout << "Solution: " << *out << std::endl;
+    std::cout << "Time elapsed: " << timer->getElapsedTime() << "ms" << std::endl << std::endl;
+    delete timer;
 
+    // Multithreaded
+    std::cout << "Running example with 2 threads" << std::endl;
+    timer = new TimeDifference();
+    timer->begin();
 
     long* out1 = new long;
     long* out2 = new long;
-
     std::thread t1(sum, 0, numberToSumUp/2, out1);
     std::thread t2(sum, numberToSumUp/2 + 1, numberToSumUp, out2);
-
     t1.join();
     t2.join();
 
     std::cout << "Solution: " << *out1 + *out2 << std::endl;
-
     timer->end();
+    std::cout << "Time elapsed: " << timer->getElapsedTime() << "ms" << std::endl << std::endl;
+    delete timer;
 
+    // 3 threads
+    std::cout << "Running example with 3 threads" << std::endl;
+    timer = new TimeDifference();
+    timer->begin();
+
+    long* out4 = new long;
+    long* out5 = new long;
+    long* out6 = new long;
+    std::thread t4(sum, 0, numberToSumUp/3, out4);
+    std::thread t5(sum, numberToSumUp/3 + 1, 2 * numberToSumUp/3, out5);
+    std::thread t6(sum, 2 * numberToSumUp/3 + 1, numberToSumUp, out6);
+    t4.join();
+    t5.join();
+    t6.join();
+
+    std::cout << "Solution: " << *out4 + *out5 + *out6 << std::endl;
+    timer->end();
     std::cout << "Time elapsed: " << timer->getElapsedTime() << "ms" << std::endl;
     return 0;
 }
